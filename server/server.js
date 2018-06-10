d7e08dc2e715cc4fef95d73468f937465ab19106
Shapeinabox <shapeinabox@gmail.com>
2018-06-10 17:14:42 +0200

    send messages to only people in same room

--- server/server.js
+++ server/server.js
@@ -38,14 +38,19 @@ io.on('connection', (socket) => {
     });
 
     socket.on('createMessage', (message, callback) => {
-        // console.log(message);
-        io.emit('newMessage', generateMessage(message.from, message.text));
-        callback('This is from the server');
+        var user = users.getUser(socket.id);
+        if(user && isRealString(message.text)){
+            io.to(user.room).emit('newMessage', generateMessage(user.name, message.text));
+            callback('This is from the server');
+        }
     })
 
     socket.on('createLocationMessage', (coords, callback) => {
-        io.emit('newLocationMessage', generateLocationMessage('Admin', coords));
-        callback();
+        var user = users.getUser(socket.id);
+        if(user){
+            io.to(user.room).emit('newLocationMessage', generateLocationMessage(user.name, coords));
+            callback();
+        }
     });
 
     socket.on('disconnect', () => {
